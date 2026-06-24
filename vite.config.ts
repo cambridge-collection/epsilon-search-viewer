8724d7ed6c4fb689c01a0fc7575c2d36ab80dd22
Michael Hawkins <mjh39@cam.ac.uk>
2026-06-24 14:45:16 +0100

    chore(deps): update dependencies and upgrade Vite to 8

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,11 @@ import del from "rollup-plugin-delete";
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
   ],
   resolve: {
     alias: {
